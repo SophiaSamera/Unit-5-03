500fafc854265415696a68130dea134d690f75ae
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-11 14:56:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function buttonOn() {
+  document.getElementById("paragraph").innerHTML = "Poof!";
+  document.getElementById("pets").src = "./Images/doggi.png";
+  document.getElementById("background").style.backgroundColor = "pink";
+  document.getElementById("myDIV").style.display = "none";
+}
