b2767018b2de7c7259867f502f3d422c74952272
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-11 15:07:47 +0000

    stuff in javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function buttonOn() {
-  document.getElementById("paragraph").innerHTML = "Poof!";
-  document.getElementById("pets").src = "./Images/doggi.png";
-  document.getElementById("background").style.backgroundColor = "pink";
-  document.getElementById("myDIV").style.display = "none";
+function buttonOn () {
+  document.getElementById('paragraph').innerHTML = 'Poof!'
+  document.getElementById('pets').src = './Images/doggi.png'
+  document.getElementById('background').style.backgroundColor = 'pink'
+  document.getElementById('mydiv').style.display = 'none'
 }
